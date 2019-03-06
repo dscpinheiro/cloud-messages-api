cf75a45cd4b7aac8fb83e96fbbfd7bcf6327e9d7
Daniel Schmidt Pinheiro <dscpinheiro@gmail.com>
2019-03-06 06:01:35 -0500

    Added the implementation for the IsPalindrome function

--- Messages.Api/Helpers/StringExtensions.cs
+++ Messages.Api/Helpers/StringExtensions.cs
@@ -1,17 +1,48 @@
 using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Messages.Api.Helpers
 {
     public static class StringExtensions
     {
-        public static bool IsPalindrome(this string word)
+        public static bool IsPalindrome(this string message)
         {
-            if (string.IsNullOrWhiteSpace(word))
+            if (string.IsNullOrWhiteSpace(message))
             {
-                throw new ArgumentNullException(nameof(word));
+                throw new ArgumentNullException(nameof(message));
             }
 
-            return false;
+            // Filter the message to include only letters and numbers
+            var stringBuilder = new StringBuilder();
+            foreach (var letter in message)
+            {
+                if (char.IsLetterOrDigit(letter))
+                {
+                    stringBuilder.Append(letter);
+                }
+            }
+
+            var filtered = stringBuilder.ToString();
+            if (string.IsNullOrWhiteSpace(filtered))
+            {
+                return false;
+            }
+
+            for (var i = 0; i < filtered.Length / 2; i++)
+            {
+                // Converting the characters to strings is not the most efficient method,
+                // but it allows comparison using of different cultures: https://stackoverflow.com/a/1394898
+                var leftLetter = filtered[i].ToString();
+                var rightLetter = filtered[filtered.Length - i - 1].ToString();
+
+                if (!string.Equals(leftLetter, rightLetter, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
