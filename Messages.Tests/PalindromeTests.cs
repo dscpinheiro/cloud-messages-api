1a56dca9ad27d1ca5d27dcbdc59a35ab6b309cec
Daniel Schmidt Pinheiro <dscpinheiro@gmail.com>
2019-03-05 20:52:21 -0500

    Added initial unit tests

--- Messages.Tests/PalindromeTests.cs
+++ Messages.Tests/PalindromeTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Messages.Api.Helpers;
 using Xunit;
 
@@ -7,17 +8,55 @@ namespace MessagesTests
     public class PalindromeTests
     {
         [Fact]
-        public void GivenANullString_ThrowsException()
+        public void GivenANullString_ItThrowsException()
         {
-            string word = null;
-            Assert.Throws<ArgumentNullException>(() => word.IsPalindrome());
+            string message = null;
+            Assert.Throws<ArgumentNullException>(() => message.IsPalindrome());
         }
 
         [Fact]
-        public void GivenAnEmptyString_ThrowsException()
+        public void GivenAnEmptyString_ItThrowsException()
         {
-            var word = string.Empty;
-            Assert.Throws<ArgumentNullException>(() => word.IsPalindrome());
+            var message = string.Empty;
+            Assert.Throws<ArgumentNullException>(() => message.IsPalindrome());
+        }
+
+        [Fact]
+        public void GivenANonPalindromeMessage_ItReturnsFalse()
+        {
+            var message = "this is not a palindrome";
+            Assert.False(message.IsPalindrome());
+        }
+
+        [Fact]
+        public void GivenAPalindromeWord_ItReturnsTrue()
+        {
+            var messages = new List<string> { "madam", "racecar", "arara" };
+            foreach (var message in messages)
+            {
+                Assert.True(message.IsPalindrome());
+            }
+        }
+
+        [Fact]
+        public void GivenAOneLetterWord_ItReturnsTrue()
+        {
+            var message = "x";
+            Assert.True(message.IsPalindrome());
+        }
+
+        [Fact]
+        public void GivenASequenceOfRepeatedNumbers_ItReturnsTrue()
+        {
+            var message = "1111111111";
+            Assert.True(message.IsPalindrome());
+        }
+
+        [Fact]
+        public void GivenASequenceOfRandomNumbers_ItReturnsFalse()
+        {
+            var message = "865357943037344";
+            Assert.False(message.IsPalindrome());
         }
     }
 }
