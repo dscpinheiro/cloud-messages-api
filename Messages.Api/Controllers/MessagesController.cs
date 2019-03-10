7c54ccbf0b94a7a05e408df82c0e27d998143967
Daniel Schmidt Pinheiro <dscpinheiro@gmail.com>
2019-03-10 19:19:36 -0400

    Updated api prefix

--- Messages.Api/Controllers/MessagesController.cs
+++ Messages.Api/Controllers/MessagesController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Messages.Api.Controllers
 {
-    [Route("messages"), ApiController]
+    [Route("api/messages"), ApiController]
     public class MessagesController : ControllerBase
     {
         private readonly IMessageService _messagesService;
