7840ed505ffb12a8a4168a991491e790e24ba239
Daniel Schmidt Pinheiro <3842788+dscpinheiro@users.noreply.github.com>
2021-11-10 15:44:30 -0500

    Update file to use file-scoped namespace declaration

--- Messages.Helpers/StringExtensions.cs
+++ Messages.Helpers/StringExtensions.cs
@@ -2,50 +2,49 @@
 using System.Globalization;
 using System.Text;
 
-namespace Messages.Helpers
+namespace Messages.Helpers;
+
+public static class StringExtensions
 {
-    public static class StringExtensions
+    public static bool IsPalindrome(this string message)
     {
-        public static bool IsPalindrome(this string message)
+        if (string.IsNullOrWhiteSpace(message))
         {
-            if (string.IsNullOrWhiteSpace(message))
-            {
-                throw new ArgumentNullException(nameof(message));
-            }
+            throw new ArgumentNullException(nameof(message));
+        }
 
-            // Filter the message to include only letters and numbers
-            var stringBuilder = new StringBuilder();
-            foreach (var letter in message)
+        // Filter the message to include only letters and numbers
+        var stringBuilder = new StringBuilder();
+        foreach (var letter in message)
+        {
+            if (char.IsLetterOrDigit(letter))
             {
-                if (char.IsLetterOrDigit(letter))
-                {
-                    stringBuilder.Append(letter);
-                }
+                stringBuilder.Append(letter);
             }
+        }
 
-            var filtered = stringBuilder.ToString();
-            if (string.IsNullOrWhiteSpace(filtered))
+        var filtered = stringBuilder.ToString();
+        if (string.IsNullOrWhiteSpace(filtered))
+        {
+            return false;
+        }
+
+        // Ignore case and accented letters: https://stackoverflow.com/a/7720903
+        var culture = CultureInfo.CurrentCulture;
+        var compareOptions = CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace;
+
+        for (var i = 0; i < filtered.Length / 2; i++)
+        {
+            var leftLetter = filtered[i].ToString();
+            var rightLetter = filtered[filtered.Length - i - 1].ToString();
+
+            var areEqual = string.Compare(leftLetter, rightLetter, culture, compareOptions) == 0;
+            if (!areEqual)
             {
                 return false;
             }
-
-            // Ignore case and accented letters: https://stackoverflow.com/a/7720903
-            var culture = CultureInfo.CurrentCulture;
-            var compareOptions = CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace;
-
-            for (var i = 0; i < filtered.Length / 2; i++)
-            {
-                var leftLetter = filtered[i].ToString();
-                var rightLetter = filtered[filtered.Length - i - 1].ToString();
-
-                var areEqual = string.Compare(leftLetter, rightLetter, culture, compareOptions) == 0;
-                if (!areEqual)
-                {
-                    return false;
-                }
-            }
-
-            return true;
         }
+
+        return true;
     }
 }
